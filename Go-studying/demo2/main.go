bfcd56ef64e1768df3caa5f9a8f5a03a0bda2533
y-Li <y-li@winas.jp>
2024-10-29 01:50:30 +0900

    demo2

--- Go-studying/demo2/main.go
+++ Go-studying/demo2/main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+
+	// fmt.Print("Hello, World!")
+	// fmt.Print("Hello, World!")
+	// fmt.Print("Hello, World!")
+
+	// fmt.Println("1")
+	// fmt.Println("2")
+	// fmt.Println("3")
+
+	// fmt.Printf("Hello, World!")
+
+	// var a = "aaa" //定義したら必ず使用
+
+	// // fmt.Println(a)
+	// fmt.Printf("%v", a)
+
+	// var a int = 10
+	// var b int = 3
+	// var c int = 5
+
+	// fmt.Println("a=", a, "b=", b, "c=", c)
+	// fmt.Printf("a=%v, b=%v, c=%v", a, b, c)
+
+	a := 10
+	// b := 20
+	// c := 30
+
+	// fmt.Printf("a=%v, b=%v, c=%v\n", a, b, c)
+
+	//printffで型を指定する
+	fmt.Printf("a=%v aの方の型は%T\n", a, a)
 }
